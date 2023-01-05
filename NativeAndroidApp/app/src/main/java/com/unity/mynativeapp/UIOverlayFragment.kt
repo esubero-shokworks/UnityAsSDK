d1bd36f5b0492740054fb1f45d4fd8c9038f3253
Erick Subero <esubero@shokworks.io>
2023-01-05 10:27:43 -0400

    Unity Export and changes in Android Studio.
    - Export result from Unity.
    - Added the functions in the fragment (Android Studio).

--- NativeAndroidApp/app/src/main/java/com/unity/mynativeapp/UIOverlayFragment.kt
+++ NativeAndroidApp/app/src/main/java/com/unity/mynativeapp/UIOverlayFragment.kt
@@ -9,11 +9,13 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 
 import com.unity.mynativeapp.databinding.FragmentUIOverlayBinding
+import com.unity3d.player.UnityPlayer
 
 class UIOverlayFragment : Fragment() {
 
     private lateinit var binding: FragmentUIOverlayBinding
     private var mUnity: UnityCommunication? = null
+    private var isCameraRecording: Boolean = false
 
     private var resultPermissions =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
@@ -40,7 +42,6 @@ class UIOverlayFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         camera()
-        btns()
 //        requestPermissions()
     }
 
@@ -48,25 +49,22 @@ class UIOverlayFragment : Fragment() {
         binding.apply {
 //            cvVideo.setLifecycleOwner(viewLifecycleOwner)
             tvFlip.setOnClickListener {
-//                cvVideo.toggleFacing()
+                UnityPlayer.UnitySendMessage("Service Manager", "DoChangeARCamera", "blue")
+                println("Sending the DoChangeCamera to Unity")
+                //                cvVideo.toggleFacing()
+            }
+            lavRecordBtn.setOnClickListener(){
+                isCameraRecording = !isCameraRecording
+                if (isCameraRecording){
+                    UnityPlayer.UnitySendMessage("Service Manager", "DoStartCameraRecording", "")
+                    println("Sending the DoStartCameraRecording to Unity")
+                }
+                else{
+                    UnityPlayer.UnitySendMessage("Service Manager", "DoStopCameraRecording", "")
+                    println("Sending the DoStopCameraRecording to Unity")
+                }
             }
             ivClose.setOnClickListener {
-                mUnity?.finished()
-            }
-        }
-
-    private fun btns() =
-        binding.apply {
-            btnShowMain.setOnClickListener {
-                mUnity?.showMain()
-            }
-            btnSendMsg.setOnClickListener {
-                mUnity?.sendMsg()
-            }
-            btnUnload.setOnClickListener {
-                mUnity?.unload()
-            }
-            btnFinish.setOnClickListener {
                 mUnity?.finished()
             }
         }
